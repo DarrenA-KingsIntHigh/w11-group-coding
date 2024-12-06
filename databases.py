86141fa254363b6935f7349fafb6d399d1a31952
DarrenA-KingsIntHigh <Darren.Ackerman260@kingsinterhigh.co.uk>
2024-12-06 15:43:32 +0000

    printable database

--- databases.py
+++ databases.py
@@ -2,33 +2,40 @@ import csv
 import os
 
 class Database():
-  database:list
-  feilds:list
-  def __init__(self, filepath:str, feilds:list):
-      with open(os.path.dirname(os.path.realpath(__file__))+'/'+filepath, "r") as file:
-          reader = csv.DictReader(file)
-              for line in reader:
-                  self.database.append(line)
+    database:list = []
+    feilds:list = []
+    def __init__(self, filepath:str, feilds:list):
+        with open(os.path.dirname(os.path.realpath(__file__))+'/'+filepath, "r") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                self.database.append(line)
 
-      self.feilds = feilds
+        self.feilds = feilds
 
-  def __del__(self):
-    pass
+    def __del__(self):
+        pass
 
-  def __str__(self):
-    pass
-    
-  def search(self):
-    pass
+    def __str__(self):
+        text = ""
+        for line in self.database:
+            text += line["Item"]+" | "+line["Category"]+" | £"+line["Price"]+"\n"
+        return text
 
-  def add(self):
-    pass
+    def search(self):
+        pass
 
-  def remove(self):
-    pass
+    def add(self):
+        pass
 
-  def edit(self):
-    pass
+    def remove(self):
+        pass
+
+    def edit(self):
+        pass
 
 stock = Database("stock.csv",[])
 customers = Database("customers.csv",[])
+
+
+if __name__ == "__main__":
+    print(stock)
